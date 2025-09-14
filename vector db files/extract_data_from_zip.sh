a10cb8dcba02ec4e53e5d49c9b2717b01af7b6c2
Noam-Sasson <noamsason2@gmail.com>
2025-09-14 14:09:24 +0300

    Mandatory files for DB creation (no need to run)

---
+++ vector db files/extract_data_from_zip.sh
@@ -0,0 +1,9 @@
+echo "Starting extraction PMC data from zip files..."
+# tar -xzf pmc-00.tar.gz -C /home/student/project/raw_data
+# echo "Extraction complete for PMC-00."
+tar -xzf pmc-01.tar.gz -C /home/student/project/raw_data
+echo "Extraction complete for PMC-01."
+# tar -xzf pmc-02.tar.gz -C /home/student/project/raw_data
+# echo "Extraction complete for PMC-02."
+# tar -xzf pmc-03.tar.gz -C /home/student/project/raw_data
+# echo "Extraction complete for PMC-03."
